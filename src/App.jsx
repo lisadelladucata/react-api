653e59f945c2389417d9aa16b182dbf5fcd1bcd3
lisadelladucata <lisa.delladucata@gmail.com>
2025-02-06 23:19:05 +0100

    feat:add-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const initialFormData = {
+  name: "",
+  race: "",
+  ki: "",
+  image: "",
+  check: false,
+};
+
 export default function App() {
   const [characters, setCharacters] = useState([]);
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleFormField = (value, fieldName) => {
+    setFormData((currentState) => ({
+      ...currentState,
+      [fieldName]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newCharacter = {
+      id: characters[characters.length - 1].id + 1,
+      name: formData.name,
+      race: formData.race,
+      ki: formData.ki,
+      image: formData.image,
+      check: formData.check,
+    };
+    setCharacters((currentState) => [...currentState, newCharacter]);
+    setFormData(initialFormData);
+  };
+
+  const handleDelete = (characterId) => {
+    setCharacters((currentState) =>
+      currentState.filter((character) => character.id !== characterId)
+    );
+  };
 
   const fetchPosts = () => {
     axios.get("https://dragonball-api.com/api/characters").then((res) => {
@@ -40,7 +76,11 @@ export default function App() {
               <>
                 <div key={character.id} className="character">
                   <div className="character-img">
-                    <img src={character.image} alt="img" />
+                    {character.image ? (
+                      <img src={character.image} alt="img" />
+                    ) : (
+                      <img src="path/to/default/image.jpg" alt="default" />
+                    )}
                   </div>
                   <div className="character-info">
                     <h2 className="character-name">{character.name}</h2>
@@ -51,9 +91,67 @@ export default function App() {
                     </div>
                   </div>
                 </div>
+                <div className="delete-btn">
+                  <button
+                    type="button"
+                    onClick={() => handleDelete(character.id)}>
+                    X
+                  </button>
+                </div>
               </>
             );
           })}
+        </div>
+        <div className="form">
+          <h3>Aggiungi una Card</h3>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="title">Nome del Personaggio</label>
+              <input
+                type="text"
+                value={formData.name}
+                onChange={(e) => {
+                  handleFormField(e.target.value, "name");
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor="img">Url immagine</label>
+              <input
+                id="img-form"
+                type="text"
+                value={formData.image}
+                onChange={(e) => handleFormField(e.target.value, "img")}
+              />
+            </div>
+            <div>
+              <label htmlFor="race">Razza</label>
+              <input
+                id="race"
+                type="text"
+                value={formData.content}
+                onChange={(e) => handleFormField(e.target.value, "race")}
+              />
+            </div>
+            <div>
+              <label htmlFor="ki">KI</label>
+              <input
+                id="ki"
+                type="number"
+                value={formData.content}
+                onChange={(e) => handleFormField(e.target.value, "ki")}
+              />
+            </div>
+            <div>
+              <label htmlFor="checkbox">Pubblica</label>
+              <input
+                type="checkbox"
+                checked={formData.check}
+                onChange={(e) => handleFormField(e.target.checked, "check")}
+              />
+            </div>
+            <button type="submit">Invia</button>
+          </form>
         </div>
       </div>
     </>
